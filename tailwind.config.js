27d0f3a1ba59010df0538a681c73b36edf34eda4
thotenn <josenriqueee@gmail.com>
2022-04-13 03:48:26 -0400

    .

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    content: [
+      './public/**/*.html',
+      './src/**/*.js',
+    ],
+    darkMode: 'media', // or 'media' or 'class'
+    theme: {
+      fontFamily: {
+        iflower: ['Indie Flower', 'Open Sans']
+      },
+      rotate: {
+        '-180': '-180deg',
+          '-90': '-90deg',
+         '-45': '-45deg',
+          '0': '0',
+          '45': '45deg',
+          '90': '90deg',
+         '135': '135deg',
+          '180': '180deg',
+         '270': '270deg',
+         '405': '405deg'
+      },
+      extend: {
+      },
+    },
+    variants: {
+      extend: {
+      },
+    },
+    plugins: [],
+  }
